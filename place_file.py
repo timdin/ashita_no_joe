46ec457e69f590cad717e468317ca0e87b0624ce
chanel-chen <chanel982@gmail.com>
2018-01-25 15:41:39 +0800

    upload file via ftp

--- place_file.py
+++ place_file.py
@@ -1,10 +1,13 @@
-import ftplib
+import ftplib, os, shutil, sys
 
 
-class ftp():
+class Ftp():
 
-    def __init__(self, user_name, password):
-        self.ftp_login(user_name, password)
+    def __init__(self, user_name="", password=""):
+        if user_name and password:
+            self.ftp_login(user_name, password)
+        else:
+            raise Exception("User name and password should not be empty!")
 
     def ftp_login(self, user_name, password):
         # login
@@ -19,23 +22,26 @@ class ftp():
 
     def ftp_upload(self, cid, local_path = "C:\Automation\Upload File"):
         # go to the correct path to upload EDI files
-        qfarok_path = '/prod/edicomm/v4/'+cid+'/in'
+        #TODO: if there are more then 1 cids in string "cid", then separate with ","
+        qfarok_path = '/prod/edicomm/v4/' + cid + '/in'
         if self.session.pwd() != qfarok_path:
             self.session.cwd(qfarok_path)
         print "PATH: "+self.session.pwd()
-        """
-        # file to send
-        file = open('kitten.jpg','rb')
-        # send the file
-        session.storbinary('STOR kitten.jpg', file)
-        
-        # close file and FTP
-        file.close()     
-        """
+
+        # send file
+        local_file_list = os.listdir(local_path)
+        print "Local Files: " + str(local_file_list)
+        for local_file in local_file_list:
+            ftp_file = qfarok_path + '/' + local_file
+            f = open(local_path + '/' + local_file, "rb")
+            a = "STOR " + ftp_file
+            self.session.storbinary(a, f)
+            print "Transfer completed for " + local_file
+            f.close()
 
     def disconnect(self):
         self.session.quit()
-        print "Logged out"
+        print "Disconnected"
 
 
 if __name__ == '__main__':
@@ -46,11 +52,6 @@ if __name__ == '__main__':
     password = 'AsdAsd4589===='
     cid = 'VAH60142'
 
-    print "START placing file..."
-    uploader = ftp(user_name, password)
-    # ftp().ftp_upload(ftp().ftp_login(user_name, password), cid)
-    uploader.ftp_login(user_name, password)
-
+    uploader = Ftp(user_name, password)
+    uploader.ftp_upload(cid)
     uploader.disconnect()
-    # uploader.ftp_upload(cid)
-    print "FINISH placing file..."
